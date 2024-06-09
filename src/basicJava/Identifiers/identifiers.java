4f79962c2fe4b4ae50c37ad69d7da031be5c01c0
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2024-06-09 13:29:53 +0530

    style: enable Typename(#5214)

---
+++ src/basicJava/Identifiers/identifiers.java
@@ -0,0 +1,24 @@
+A name in java program is conserned as Identifiers, which can be use for identification.
+Eg:- Class name, Method name , Level name etc.
+
+
+
+class Test {  // Test is identifiers 
+	      public static void main(String[] args) { //main,String,args is identifiers
+			int x=10; // x is identifiers
+	      }
+}
+
+	     
+//Character can be use as Identifiers:- [ a to z , A to Z , 0 to 9, $, _ ] 
+
+Eg:- total_num right 
+     total#   (compile time error) wrong 
+
+     total123 right
+     123total wrong
+    
+java Language is case sensative language.
+
+
+
