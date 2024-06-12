7df24a55d4b353621e679d06f66ec657d65a97a3
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2024-06-12 13:15:03 +0530

    primative dt

---
+++ src/basicJava/Identifiers/primativedatypes.java
@@ -0,0 +1,37 @@
+Primative data types(8):- 1.Numberic Data types   2.Non-Numeric Data types
+			  |
+			  |
+			  |
+	a) Integral datatypes   b) Floating point
+	       |		data types
+	|                         |
+	|-byte                    |
+	|-short			  |-- float
+	|-int 			  |
+	|-long                    |-- double
+
+
+
+
+
+
+			2.Non-Numberic Data types
+			  |
+			  |--char
+			  |
+			  |--boolean
+
+
+
+byte:- 
+  size-- 1 byte(8 bits)
+
+	max_value:- +127
+	min_value:- -128
+
+byte is the best choice if we want to handle data in term of stream either from the file or from the network (file Supported form or netword supported form is the bbyte:- 
+	
+
+
+
+
