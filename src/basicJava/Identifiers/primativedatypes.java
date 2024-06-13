469445b006538aeb2c044d9f6554192adee67b28
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2024-06-13 19:27:54 +0530

    commit some more datatype

--- src/basicJava/Identifiers/primativedatypes.java
+++ src/basicJava/Identifiers/primativedatypes.java
@@ -24,14 +24,48 @@ Primative data types(8):- 1.Numberic Data types   2.Non-Numeric Data types
 
 
 byte:- 
-  size-- 1 byte(8 bits)
+  size-- 1 bytes(8 bits)
 
 	max_value:- +127
 	min_value:- -128
 
 byte is the best choice if we want to handle data in term of stream either from the file or from the network (file Supported form or netword supported form is the bbyte:- 
-	
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------	
+		
+short:- 
+   size-- 2 bytes(16 bits)
+
+   	max_value:- +32767
+	min_value:- -32768
+
+	Range:- -2^15 to 2^15-1
 
 
+short datatype is best sutable for 16 bit processor like 8085, but these processor are completly outdated and hence corespoinding sort data is also outdated data type.
 
 
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+int:- 
+    size: 4 bytes(32 bits)
+    
+       max_value:- +2147483647
+       min_value:- -2147483648
+
+       Range:- -2^31 to 2^31-1
+
+The most comonly data type is use in Java is int.
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+long:- 
+    size: 8 bytes(64 bits)
+    
+    Range:- -2^63 to 2^63-1
+
+Eg. l=126.000 x 60 x 60 x 24 x 1000 miles
+
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
