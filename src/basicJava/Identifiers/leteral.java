495108cb50de25dcadadce0f2781ade384623469
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2024-06-15 13:35:48 +0530

    add Literal

---
+++ src/basicJava/Identifiers/leteral.java
@@ -0,0 +1,29 @@
+Literal
+
+Eg. int  x = 10;
+  
+
+Here:-
+int is datatype/Keyword
+x is Name of variable/identifier
+10 is constant value/literal
+
+
+A Constant value which can be assigned to the variable which called lateral.
+
+Integral Literal:-
+
+1. decimal literal(base-10):-(0 t0 9)
+			      int x= 10;------decimal form
+			      
+
+2. Octal form (base-8):- (0 to 7)
+
+			  int x = 010;  ------// Prefix with zero
+
+3. Hexa decimal form (base-16):- (0 to 9 ,a to f)
+
+				  int x = 0x10; ----// Prefix with 0x or 0x
+
+
+
