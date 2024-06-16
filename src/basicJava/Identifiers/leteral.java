57e2536aa5e1c6b0ca674b28181dde57406b4e57
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2024-06-16 13:05:58 +0530

    valid letaral

--- src/basicJava/Identifiers/leteral.java
+++ src/basicJava/Identifiers/leteral.java
@@ -27,3 +27,51 @@ Integral Literal:-
 
 
 
+
+class Test {
+	public static void main(String[] args) {
+
+		int x=10;
+		int y=010;
+		int z=0x10;
+
+		System.out.println(x+"---"+y+"---"+z);  //output:- 10---8--16
+	}
+}
+
+
+we can write like this:- 
+
+int x=10;
+long l=10l;
+long l=10;
+byte b= 10;
+byte b=127;
+short s=32767;
+float f=123.456F;
+double d= 123.456;
+double d= 123.456d;
+double d= 0123.456;
+double d= 0xFace;
+double d= 0786.0;
+double d=10;
+double d=0777;
+double d= 1.2e3;
+float f= 1.2e3F;
+boolean b=true;
+char ch='a';
+
+char ch = 97;
+char ch = 0xFace;
+char ch = 0777;
+char ch = 65535;
+
+
+
+
+
+
+
+
+
+escape character:- 
