1e165580ce73e6da6ced7467742af5b4c603506f
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2024-06-16 13:17:36 +0530

    escape character

--- src/basicJava/Identifiers/leteral.java
+++ src/basicJava/Identifiers/leteral.java
@@ -74,4 +74,58 @@ char ch = 65535;
 
 
 
-escape character:- 
+escape character:-
+Every escape character is a valid char Literal.
+
+Eg.
+char ch = '\n';
+char ch = '\t';
+
+
+Escape Character----------description
+
+\n ----------------------New line
+\t ----------------------Horijontad Tab
+\r ----------------------Carriage return
+\b ----------------------Back space
+\f ----------------------Form Feed
+\' ----------------------Single quote
+\" ----------------------Double quote
+\\ ----------------------Back Slash
+
+
+
+
+
+For integral datatype untill 1.6 version we can
+specified literal value in the following ways:-
+1). decimal
+2). Octal
+3). Hexadecimal form
+
+
+But 1.7 version onword we can specified literal value even in Binary form also allow digits are 0,1.
+// Prefix with 0b,0B
+
+Eg:- int x = 0B1111;
+     System.out.println(x); //15
+			    
+
+
+
+
+we can write like this :-
+
+double d= 123456.789;
+
+
+double d= 1_23_456.7_8_9;
+double d= 123_456.7_8_9;
+
+double d= 1__23_4_5__6.7_8_9;
+double d= 1___2__3_4_5_6.7_8_9;
+
+
+
+eg. float f= 10L;
+System.out.println(f); //10.0
