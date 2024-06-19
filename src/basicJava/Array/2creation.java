739a3b2748db5c782c40982675c1a983e60fafd6
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2024-06-19 15:00:26 +0530

    2D array_creation

--- src/basicJava/Array/2creation.java
+++ src/basicJava/Array/2creation.java
@@ -32,3 +32,40 @@ The max allowed array size in java is 2147483647 which is the maxmimum value of 
 int[] x = new int[2147483647];
 
 int[] x = new int[2147483647]; //C.E:- integer number too large
+
+
+
+
+2D Array Creation:- In java 2D Array not implemented by matrix style some peoplee followed Array of Arrays 
+approach for multidimensional Array creation the main Advantage of this Array is memory utilization will be improved.
+
+Eg1.
+
+int[][] x = new int[2][];
+x[0] = new int[2];
+x[1] = new int[3];
+
+
+
+Eg2.
+
+int[][][] x = new int[2][][];
+x[0]  = new int[3][];
+x[0][0] = new int[1];
+x[0][1] = new int[2];
+x[0][2] = new int[3];
+x[1] =  new int[2][2];
+
+
+In the following question which one is correct:-
+
+int[] a = new int[]; //wrong
+int[] a = new int[3]; //right
+int[][] a = new int[][]; //wrong
+int[][] a = new int[3][]; //right
+int[][] a = new int[][4]; //wrong
+int[][] a = new int[3][4]; //right
+int[][][] a = new int[3][4][5]; //right
+int[][][] a = new int[3][4][]; //right
+int[][][] a = new int[3][][5]; //wrong
+int[][][] a = new int[][4][5]; //wrong
