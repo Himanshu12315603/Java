a978309fea7c30c6cad093f24c6d4867564c7604
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2024-06-19 14:46:35 +0530

    1-DArray Creation

--- src/basicJava/Array/2creation.java
+++ src/basicJava/Array/2creation.java
@@ -5,3 +5,30 @@ int[] x = new int[];  // wrong
 
 int[] x = new int[6]; //right
 		     
+int[] x = new int[0]; //right
+
+int[] x = new int[-3]; //R.E
+
+To specify allow datatypes are byte,short,char,int if you
+are trying to specify any other type then we will get C.E.
+
+
+int[] x = new int[10];
+
+int[] x = new int['a'];
+
+byte b=20;
+int[] x = new int[b];
+
+short s=30;
+int[] x = new int[s];
+
+int[] x = new int[10l]; // C.E:- PLP
+			// found: long
+			// required: int
+
+The max allowed array size in java is 2147483647 which is the maxmimum value of int datatype.
+
+int[] x = new int[2147483647];
+
+int[] x = new int[2147483647]; //C.E:- integer number too large
