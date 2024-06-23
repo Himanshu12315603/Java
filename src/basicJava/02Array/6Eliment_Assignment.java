88d78d0e6d2aca07df389073dd05c25978721cf1
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2024-06-23 15:46:31 +0530

    create Assignmentfile

---
+++ src/basicJava/02Array/6Eliment_Assignment.java
@@ -0,0 +1,76 @@
+Case1:- In the case of Primitive type Arrays As Array Eliments we can provide
+Any type which cn be implicitly promoted to declared type.
+
+Eg1.
+int[] x = new int[5];
+
+x[0] = 10;
+x[1] = 'a';
+byte b = 20;
+x[2] = b;
+short s = 30;
+x[3] = 8;
+x[4] = 10l -----> //CE: Possiple loss of precision 
+		  //found: long
+		  //required: int
+
+Eg2. In the case of float type Array the allowed datatypes are 
+byte--->Short,int,long,float.
+
+
+
+
+
+
+Case2:-In the case of object type Arrays as Array Elements we can provide Either declare type O
+Objects or its child class Objects.
+
+
+Eg1.   
+	object[] a = new object[10];
+	a[0] = new object();
+	a[1] = new string("durga");
+	a[2] = new Integer(10);
+
+
+
+Eg2.
+	Number[] n = new Number[10];
+
+	n[0] = new Integer(10);
+	n[1] = new Double(10.5);
+	n[2] = new String("durga"); // CE: incomplite types
+				    // found: j.l.String
+				    // required: j.l. Number
+
+
+Eg3. 
+	Runnable[] r = new Runnable[10];
+
+	r[0] = new Thread();
+	r[1] = new String("durga"); // CE: incomplatile type
+				    // found: j.l.String
+				    // required: j.l. Runnable
+
+
+
+
+
+Array Type 				Allowed element Types
+---------------------------------------------------------------------------------------------------
+Primitive Arrays			Any type which can be implicitly promoted to declared type.
+
+
+Object type Arrays			Either declared type or its child class objects.
+
+
+Abstract class type Arrays		Its child class objects.
+
+
+interface type Array 			Its Implementation class objects are allowed.
+
+
+
+
+
+
