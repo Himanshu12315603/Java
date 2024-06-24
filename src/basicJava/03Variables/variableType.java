bdf3e2f432e9fb547cfb3431da4c87135fbe0251
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2024-06-24 15:57:10 +0530

    variabletype

---
+++ src/basicJava/03Variables/variableType.java
@@ -0,0 +1,13 @@
+Types of variables
+
+Division1:- Based on type of value represented by a variable all
+variable are divided into 2 types:-
+
+1. Primitive variable can be used to represent primitive values.
+
+Eg. int x = 10;
+
+
+2. Reference variable can be used to refer objects.
+
+Eg. Student s = new Student();
