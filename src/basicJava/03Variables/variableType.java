0af8a64ca350f59d1e0a92871e2a9ad586c983ed
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2024-06-25 14:54:15 +0530

    add division2

--- src/basicJava/03Variables/variableType.java
+++ src/basicJava/03Variables/variableType.java
@@ -11,3 +11,15 @@ Eg. int x = 10;
 2. Reference variable can be used to refer objects.
 
 Eg. Student s = new Student();
+
+
+
+Division2:- Based on position of declaration and Behaviour all 
+variables are divided intoo 3 types.
+
+1). Instance Variables
+2). Static Variables
+3). Local Variables
+
+
+
