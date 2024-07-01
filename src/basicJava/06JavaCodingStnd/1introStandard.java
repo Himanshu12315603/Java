1ad0e760cc2ba4d94596506ea40ce09a2900d427
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2024-07-01 15:17:55 +0530

    Coding Stndard

---
+++ src/basicJava/06JavaCodingStnd/1introStandard.java
@@ -0,0 +1,33 @@
+Java coding Standard:-
+
+Whenever we are writing java code it is highly recoomanded to follow coding standard.
+
+Whenever we are writing any component its name should reflect the purpose of the
+Component(functionality).
+
+The main advantage of this approach is readibility and maintainability of the code will be improved.
+
+
+Eg. 
+class A 
+{
+	public int m1(int x, int y)
+	{
+		return x+y;
+	}
+}
+
+	Lpu standard
+
+_____________________________________________-
+package com.durgasoft.scjp;
+
+public class calculator {
+
+	public static int add(int num1, int num2)
+	{
+		return num1 + num2;
+	}
+}
+
+	Hi-tech city standard
---
+++ src/basicJava/06JavaCodingStnd/2CodingStandard.java
@@ -0,0 +1,55 @@
+1.Coding Standart for classes:- Usully class names are nouns should start with 
+	uppercase character and if it contain multiple words every inner word should
+	start with uppercase character.
+
+	String
+	StringBuffer
+	Account
+	Dog
+	------------ Noun
+
+
+
+
+2. Coding Standards for interaface:- Usully interaface names are adjectives should
+	start with Uppercase character, if it contains multiple worlds every inner
+	words Starts with Uppercase character.
+
+
+	Runnable
+	Serializable
+	Comparable
+	------------- > Adjective
+
+
+
+
+3. Coding Standard for method:- Usully methods names are either verbs or verb noun
+		combination. Should starts with Lowercase alphabet symbols and if
+		it contain multiple word then Every inner word should starts with 
+		Uppercase Character(Cammel Case Conversion).
+
+
+		print()
+		sleep()					getName()
+		run()					setSalary()
+		eat()					---------------verb-noun Combination
+		start()
+	---------------verbs
+
+
+4. Coding Standard for variable:- Usully variable names are nouns should starts with Lowercase
+		alphabet symbol and if it contain multiple words then every inner words starts
+		with Uppercase character(camel case conversion).
+
+
+		name
+		age
+		salary 
+		mobileNumbers
+		---------------nouns
+
+
+
+
+5. Coding Standard for Constant:- 
