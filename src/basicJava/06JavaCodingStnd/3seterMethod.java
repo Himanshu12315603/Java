8ce2c671ab110e70b8085bbfc1d457a5c3723ee7
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2024-07-02 13:05:48 +0530

    seterMethod

---
+++ src/basicJava/06JavaCodingStnd/3seterMethod.java
@@ -0,0 +1,31 @@
+Syntax for Seter method:-
+
+1. it should be public method.
+2. The return type should be void.
+3. Method name should be prefixed with set.
+4. It should take some argument that it should not be no argument method.
+
+	
+
+	public void set Name(String name) {
+		
+	}
+
+
+
+
+Syntax for getter method:-
+
+1. It should be public method.
+2. The return type should not be void.
+3. Method name should prefix with get. 
+	it should not take any argument.
+
+		
+	public String getName() {
+
+	}
+
+
+Note:- For boolean property getter method name can be prefixed with 
+	either get or is, but recommanded to use is.
