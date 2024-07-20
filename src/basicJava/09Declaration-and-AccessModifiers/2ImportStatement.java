27401838545b31d72012cf8d8bb4603814bdd1e3
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2024-07-20 23:43:17 +0530

    Import Statemnt

--- src/basicJava/09Declaration-and-AccessModifiers/2ImportStatement.java
+++ src/basicJava/09Declaration-and-AccessModifiers/2ImportStatement.java
@@ -54,15 +54,17 @@ Best Sutable for High-tech city            Student where typing is importent.
 where readibility is important.
 
 
+____________________________________________________________________________________________
 
 Case2:- Which of the following import Statements are meaningfull:-
 
-import java.util.ArrayList;   //right
+import java.util.ArrayList;    //right
 import java.util.ArrayList.*;  //wrong
-import java.util.*;             //right
-import java.util;               //wrong
+import java.util.*;            //right
+import java.util;              //wrong
 
 
+____________________________________________________________________________________________
 
 
 Case3:- Consider the following Code:-
@@ -80,6 +82,7 @@ use fullyCollified name.
 
 
 
+____________________________________________________________________________________________
 
 Case4:- 
 
@@ -92,4 +95,88 @@ class Test {
   }
 }
 
+Note:- Even in the case of List also we may get same ambiguoti problem because
+  it is available in the both util and awt package.
+
+
+____________________________________________________________________________________________
+
+Case5:- While resolving class name compilar will always give the president 
+in the following order.
+
+1.Explicit class import 
+2.classes present in CWD(default pkg)
+3.Implicit class import 
+
+Eg.             
+import java.util.Date;
+import java.sql.*;
+
+class Test {
+  public static void main(String[] args) {
+    Date d = new Datee();
+    System.out.println(d.getClass().getName());
+  }
+}
+
+In the above Example util package Date got Consider.
+
+____________________________________________________________________________________________
+Case6:- Whenever we are importing a java pkg all class and intrafaces present in 
+that pkg by default available but not subpkg classes if we want to use subpkg class 
+compulsory  we should write import Statement until subpkg level.
+
+Eg. 
+  
+To use pattern class in our program which import Statement is required.
+
+1.import java.*;
+2.import java.util.*;
+3.import java.util.regex.*; //right
+4.No import required
+
+
+____________________________________________________________________________________________
+
+Case7:- All classes and intrafaces present in the following pkges or by default
+available to every java program.
+
+ we are not required to write import Statement.
+
+ 1. java.lang
+ 2. default pkg(CWD)
+
+
+
+
+____________________________________________________________________________________________
+
+
+
+Case8:- import Statement is totaly Compile time related concept if more number of import then more 
+will be the compile time, But there is no Effect on execution time (Run time).
+
+
+____________________________________________________________________________________________
+
+Case9:- Difference between c lang #include<stdio.h> and Java lang import Statement:-
+
+In the case of C lang #include all input output header file will be loaded at beggining only (at translation time)
+  Hence it is static include.
+                    But in the case of java import Statement no .class file will be loaded at the beggining
+                    whenever we are using a particular class then only corresponding .class file will be loaded
+                    this is like dynamic include or Load on Demand or Load on fly.
+
+
+Note:- 1.5 version new feature:
+
+1. for-each loop
+2. var-arg methods
+3. Autoboxing & Autounboxing
+4. Generics
+5. Co-varient return Types
+6. Queue
+7. Annotations
+8. enum
+9. static import 
 
