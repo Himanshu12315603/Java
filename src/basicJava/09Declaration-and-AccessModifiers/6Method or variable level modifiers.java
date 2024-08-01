baa9b3406ed168ec51af2f2918bccd0b66894a39
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2024-08-01 14:30:13 +0530

    add: all methods or variable

--- src/basicJava/09Declaration-and-AccessModifiers/6Method or variable level modifiers.java
+++ src/basicJava/09Declaration-and-AccessModifiers/6Method or variable level modifiers.java
@@ -51,6 +51,7 @@ Abstract method should be available to the child classes to provide implimentati
 not available to the child classes. Hence private abstract combination is illigal for method.
 
 
+_______________________________________________________________________________________________________________
 
 4. Protected Member (The most misunderstood modifier is java) :- If a member declare as protected then we can 
 access that member anywhere with in the current package, but only child classes of outside package.
@@ -115,6 +116,139 @@ class C extends A
 
 
 
-We can access protected 
+We can access protected members from outside package only in child classes and we 
+should use that child class refrence only for Eg. from D class if we want to 
+access we should use D class Reference.
+
+Eg.
+package pack2;
+import pack1.A;
+
+class C extends A { }
+class D extends C 
+{
+  public static void main(String[] args)
+  {
+    A a = new A();
+    a.m1();                      //CE: m1() has protected access in pack1.A
+
+    C c = new C();
+    c.m1();                      //CE: m1() has protected access in pack1.A
+
+    D d = new D();
+    d.m1();                    //right 
+
+    A a1 = new C();
+    a1.m1();                     //CE: m1() has protected access in pack1.A
+
+    A a1 = new D();
+    a1.m1();                     //CE: m1() has protected access in pack1.A
+  }
+}
 
 
+
+
+
+visibility          private         default             protected           public 
+
+within the          right           right                right              right 
+same class 
+
+
+From child class    wrong           right               right               right
+of same package 
+
+
+From Non-child      wrong           right               right               right 
+class of same pkg
+  
+
+From child class    wrong           wrong               right               right 
+of outside pkg
+
+
+From Non-child      wrong           wrong               wrong               right
+class of outside pkg
+
+
+The most restricted access modifier is private the most accesible modifier is public.
+
+      
+                    private < default < protected < public 
+
+Recomanded modifier for data member(variable) is private but recomanded modifier for
+method is public.
+
+
+
+_______________________________________________________________________________________________________________
+
+6. static modifier:- static is the modifier applicable for methods and variables but not for classes.
+
+We can't declare top level class with static modifier but we can declare inner class as static 
+(such type of inner classes are called static nested classes).
+
+In the case of instance variable for every object a seprate copy will be created but in the case 
+of static variables a single copy will be created at class level and shared by every object of 
+that class.
+
+Eg. 
+class Test 
+{
+  static int x = 10; 
+  int y = 20;
+
+  public static void main(String[] args) 
+  {
+    Test t1 = new Test();
+    t1.x = 888;
+    t1.y = 999;
+
+    Test t2 = new Test();
+
+    System.out.println(t2.x+ " ---- " + t2.y);          // 888 ---- 20
+  }
+}
+
+
+We can't acces instance memberss directly from static Area but we can access
+from instance area directly.
+
+we can access  static members from both instance and static Area directly.
+
+
+So consider the following declarations:-
+
+static variable we can access from any Area, but instance variable only access from Instance Area.
+
+
+1. int x = 10;
+
+2. static int x= 10;
+
+3. public void m1() 
+    {
+      System.out.println(x);
+    }
+
+4. public static void m1() 
+    {
+      System.out.printlln(x);
+    }
+
+
+Q. within the same class which of the above declaration we can take simultenesly.
+
+
+a). 1 & 3       //right
+
+b). 1 & 4       //CE: non staic variable x cannot be referendd from a static context
+
+c). 2 & 3       //right
+
+d). 2 & 4       //right
+
+e). 1 & 2       //wrong: variable x is already defined in Test
+
+f). 3 & 4       //wrong: m1() is already defined in Test
