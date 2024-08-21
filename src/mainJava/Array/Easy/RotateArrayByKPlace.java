9a304be6acc88b15441fbc7af57dc6931f65ff69
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2024-08-21 23:21:32 +0530

    make some change

--- src/mainJava/Array/Easy/RotateArrayByKPlace.java
+++ src/mainJava/Array/Easy/RotateArrayByKPlace.java
@@ -6,10 +6,7 @@ class Main {
     k = k % n;
     if (k > n)
       return;
-    int[] temp = new int[k];
-    for (int i = 0; i < k; i++) {
-      temp[i] = arr[i];
-    }
+    int[] temp = new int[k]; for (int i = 0; i < k; i++) { temp[i] = arr[i]; }
     for (int i = 0; i < n - k; i++) {
       arr[i] = arr[i + k];
     }
@@ -45,11 +42,11 @@ end--;
 // Function to Rotate k elements to right
 public static void Rotateeletoright(int[] arr, int n, int k) {
 // Reverse first n-k elements
-Reverse(arr, 0, n - k - 1);
-// Reverse last k elements
-Reverse(arr, n - k, n - 1);
+Reverse(arr, 0, n - k - 1);         [5,4,3,2,1,6,7]
+// Reverse last k elements          
+Reverse(arr, n - k, n - 1);         [5,4,3,2,1,7,6]
 // Reverse whole array
-Reverse(arr, 0, n - 1);
+Reverse(arr, 0, n - 1);             [6,7,1,2,3,4,5]
 }
 public static void main(String args[]) {
 int[] arr = {1,2,3,4,5,6,7};
