fb23be9aeac1e490ffd9978aa65383fbbcbe1e32
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2024-09-03 20:29:21 +0530

    change: made some changes

--- src/Main Java/Array/Easy/MoveZerotoEnd.java
+++ src/Main Java/Array/Easy/MoveZerotoEnd.java
@@ -7,25 +7,53 @@ class Solution {
     MoveZero(arr);
   }
 
+  /* space complexity is high in the section */
+//  public static void MoveZero(int arr[]) {
+//    int n= arr.length;
+//    int nums[] = new int[n];
+//    int j=0;
+//    int k=0;
+//    for (int i = 0; i < n; i++) {
+//      if(arr[i]!=0) {
+//        nums[j]=arr[i];
+//        j++;
+//      }
+//      else {
+//        nums[n-1-k]=arr[i];
+//        k++;
+//      }
+//    }
+//
+//    System.out.println("After moving zero at the end");
+//    for (int i = 0; i < n; i++) {
+//      System.out.print(nums[i]+" ");
+//    }
+//  }
+
   public static void MoveZero(int arr[]) {
     int n= arr.length;
-    int nums[] = new int[n];
-    int j=0;
-    int k=0;
-    for (int i = 0; i < n; i++) {
+    int index=0;
+
+
+    for (int i = 0; i < n; i++) {       // shifting non-zero element at the first 
       if(arr[i]!=0) {
-        nums[j]=arr[i];
-        j++;
-      }
-      else {
-        nums[n-1-k]=arr[i];
-        k++;
+        arr[index]=arr[i];
+        index++;                        // And every time it will increasing
       }
     }
 
+    // Again Run a loop from index to size of array for addding zero at the end
+    for(int i=index; i<n; i++) 
+    {
+      arr[i]=0;     // After non zero element the next thing will be zero 
+    }
+
+
     System.out.println("After moving zero at the end");
     for (int i = 0; i < n; i++) {
-      System.out.print(nums[i]+" ");
+      System.out.print(arr[i]+" ");
     }
   }
+
+  
 }
