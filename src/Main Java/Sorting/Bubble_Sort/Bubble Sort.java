88d2ca02f385018c1e60a66b436991b84cb44a82
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2024-09-08 16:21:39 +0530

    add: Bubble Sort technique

---
+++ src/Main Java/Sorting/Bubble_Sort/Bubble Sort.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution 
+{
+  public static void main(String[] args) 
+  {
+    int arr[] = {1,3,2,6,4,5,8,4};
+
+    int n=arr.length;
+    BubbleSort(arr,n);
+    print_array(arr,n);
+  }
+
+  // Bubble sort method
+  public static void BubbleSort(int arr[],int n)
+  {
+
+    for(int i=0; i<n-1; i++)
+    {
+      for(int j=0; j<n-i-1; j++)
+      {
+        if(arr[j]>arr[j+1])
+        {
+          int temp=arr[j];
+          arr[j]=arr[j+1];
+          arr[j+1]=temp;
+        }
+      }
+    }
+  }
+
+  // Print the sorted array
+  public static void print_array(int arr[],int n)
+  {
+    for (int i = 0; i < n; i++) {
+      System.out.print(arr[i] + " ");  // [1 2 3 4 4 5 6 8]
+    }
+  }
+}
