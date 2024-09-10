02ad5c2d45c3dd041bf20aa429820a51e61d918f
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2024-09-10 22:44:54 +0530

    make: Binary Search

---
+++ src/Main Java/Searching/Binary_Search/Binary Search.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+
+class Solution 
+{
+  public static void main(String[] args) 
+  {
+    int arr[] = {1,4,6,10,15,20,30,36,40};
+    int target = 36;
+
+    System.out.println(binarySearch(arr,target));
+  }
+
+  static int binarySearch(int arr[],int target)
+  {
+    int n = arr.length;
+    int low=0;
+    int high=n-1;
+    
+    while(low<=high)
+    {
+      int mid=(low+high)/2;
+      
+      if(arr[mid]==target) {
+        return mid;
+      }else if(arr[mid]<target) {
+        low=mid+1;
+      }else {
+        high=mid-1;
+      }
+    }
+    return -1;
+  }
+}
