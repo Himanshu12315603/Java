d321f3fb3a07118917032bd0102071639ebc83e9
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2024-09-13 21:36:15 +0530

    Linear Search.java

---
+++ src/Main Java/Searching/Linear_Search/Linear Search.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution 
+{
+  public static void main(String[] args)
+  {
+    
+    int arr[] = {12,14,16,23,32,42,24,89};
+    int target = 32;
+    System.out.println(linearSearch(arr,target));
+  }
+
+  // Time complexity of O(n);
+
+  public static boolean linearSearch(int arr[],int target)
+  {
+    int size = arr.length;
+    for(int i=0; i<size; i++)
+    {
+      if(arr[i]==target) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
