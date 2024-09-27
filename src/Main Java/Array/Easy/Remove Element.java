d7e97482556dc51958f0d747b8222e4c0a71795b
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2024-09-27 19:04:20 +0530

    change: some of part

--- src/Main Java/Array/Easy/Remove Element.java
+++ src/Main Java/Array/Easy/Remove Element.java
@@ -1,19 +1,23 @@
 class Solution {
 
   public static void main(String[] args) {
+
     Solution sol = new Solution();
     int[] nums = {3, 2, 2, 3};
     int val = 3;
 
     int newLength = sol.removeElement(nums, val);
-    kd dfkd:w
 
     System.out.println("New length: " + newLength);
-    System.out.println("Modified array: ");
+    System.out.print("Modified array: ");
     for (int i = 0; i < newLength; i++) {
       System.out.print(nums[i] + " ");
     }
+    System.out.println();
   }
+
+
+  // we just need to print the length of the array after remove the element 
   public int removeElement(int[] nums, int val) {
     // Initialize a pointer j to keep track of the position to overwrite in the array
     int j = 0;
@@ -29,7 +33,7 @@ class Solution {
       }
     }
     // Return the new length of the array after removal
-    return j;
+    return j;   // 2
   }
 }
 
