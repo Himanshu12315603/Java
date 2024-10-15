5781228a0a9c4caad6c100c88dc126fe783e8c20
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2024-10-16 01:13:33 +0530

    add: question like celling

---
+++ src/Main Java/Searching/Binary_Search/Find Smallest Letter Greater Than Target.java
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/find-smallest-letter-greater-than-target/description/
+
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (letters[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        // The start index will point to the next greatest letter
+        return letters[start % letters.length];
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        char[] letters = {'c', 'f', 'j'};
+        char target = 'a';
+        char result = solution.nextGreatestLetter(letters, target);
+        System.out.println("The next greatest letter after '" + target + "' is: '" + result + "'");     // output :- 'c'
+    }
+}
