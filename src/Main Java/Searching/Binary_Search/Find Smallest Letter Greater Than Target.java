3ada43ba7b7b31d38e120e0f76ca6cdebe560d8b
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2024-10-17 12:10:42 +0530

    change: the logic and details

--- src/Main Java/Searching/Binary_Search/Find Smallest Letter Greater Than Target.java
+++ src/Main Java/Searching/Binary_Search/Find Smallest Letter Greater Than Target.java
@@ -1,29 +1,29 @@
-// https://leetcode.com/problems/find-smallest-letter-greater-than-target/description/
+// source page ------ https://leetcode.com/problems/find-smallest-letter-greater-than-target/description/
 
 class Solution {
-    public char nextGreatestLetter(char[] letters, char target) {
-        int start = 0;
-        int end = letters.length - 1;
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    char[] letters = {'c', 'f', 'j'};
+    char target = 'a';
+    char result = solution.nextGreatestLetter(letters, target);
+    System.out.println("The next greatest letter after '" + target + "' is: '" + result + "'");     // output :- 'c'
+  }
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
+  public char nextGreatestLetter(char[] letters, char target) {
+    int start = 0;
+    int end = letters.length - 1;
 
-            if (letters[mid] > target) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-        }
+    while (start <= end) {
+      int mid = start + (end - start) / 2;
 
-        // The start index will point to the next greatest letter
-        return letters[start % letters.length];
+      if (letters[mid] > target) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
     }
 
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        char[] letters = {'c', 'f', 'j'};
-        char target = 'a';
-        char result = solution.nextGreatestLetter(letters, target);
-        System.out.println("The next greatest letter after '" + target + "' is: '" + result + "'");     // output :- 'c'
-    }
+    // The start index will point to the next greatest letter
+    return letters[start % letters.length];  // 2%4 = 2 but 4%4 = 0
+  }
 }
