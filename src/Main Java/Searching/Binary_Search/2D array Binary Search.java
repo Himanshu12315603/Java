eb2699c1108001fb3d7dac300a2767aab4b2892f
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2024-10-22 19:16:01 +0530

    change: else part

--- src/Main Java/Searching/Binary_Search/2D array Binary Search.java
+++ src/Main Java/Searching/Binary_Search/2D array Binary Search.java
@@ -19,21 +19,22 @@ class Solution
     int r = 0;
     int c = matrix.length - 1;
 
+    // it will run row wise and column wise 
     while (r < matrix.length && c >= 0) {
 
       if(matrix[r][c] == target) 
       {
         return new int []{r, c};
       }
-      //if the number is greater then target remove the column cause the entire column would be greater then target
-      else if(matrix[r][c] > target) 
-      {
-        c--;
-      }
       // if the  number is less then target move to the next row cause the entire row would be less then target
-      else 
+      else if(matrix[r][c] < target) 
       {
         r++;
+      }
+      //if the number is greater then target remove the column cause the entire column would be greater then target
+      else 
+      {
+        c--;
       }
     } // if anything not found just return {-1, -1}
     return new int[]{-1, -1};
